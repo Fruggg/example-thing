c6a0cd539012a852ac7d421a26760b71b89bd680
Shaun <Shaunfruggie1257@gmail.com>
2022-12-19 10:17:55 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace namespacee
+{
+    class Program
+    {
+        struct Country
+        {
+            public float GDP;
+            public string name;
+            public Country(string name, float GDP)
+            {
+                this.name = name;
+                this.GDP = GDP;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            //create the instances
+            Country US = new Country("U.S.A", 2);
+            //the collection we'll use 
+            Dictionary<string, Country> collection = new Dictionary<string, Country>();
+            //add them
+            collection.Add("America", US);
+
+
+            Console.WriteLine("Choose your fate, O coward!\n\"America\"\n\"Ham\"");
+            string choice = Console.ReadLine();
+
+            //gets the correct country based on the entry. Null if the entry is invalid.
+            Country selected = collection[choice];
+            
+        }
+    }
+}
